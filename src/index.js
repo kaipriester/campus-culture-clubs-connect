3ef645272938f6b18efc3b5ceb60c03b62613766
Kai Priester <kai@priester.net>
2022-02-07 12:04:11 -0500

    page name added

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+var element = React.createElement('h1', { className: 'greeting' }, 'Campus Culture Clubs Connect (4C)');
+ReactDOM.render(element, document.getElementById('root'));
+
+ReactDOM.render(element,
   document.getElementById('root')
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
