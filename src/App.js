4ebb5e11bb30ffe43e8a79f6123a1168ef0cf56b
Kai Priester <kai@priester.net>
2022-03-28 16:18:16 -0400

    clean up history

--- src/App.js
+++ src/App.js
@@ -6,18 +6,30 @@ import MyClubs from './components/MyClubs.js';
 import MyCards from './components/MyCards.js';
 import UserLogin from './components/UserLogin.js';
 //Router
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
+  import {authenticated} from './components/LoginForm/index.js'
 
-
+function requireAuth(nextState, replace, next) {
+  if (!authenticated) {
+    replace({
+      pathname: "/userlogin",
+      state: {nextPathname: nextState.location.pathname}
+    });
+  }
+  next();
+}
 
 function App() {
+  //const [auth] = useState;
+  const [userInfo] = useState();
   return (
     <Router className="App">
-
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/myclubs" element={<MyClubs />} />
-        <Route path="/mycards" element={<MyCards />} />
+        <Route path="/myclubs" element={<MyClubs/> } onEnter={requireAuth}/>
+        <Route path="/mycards" element={<MyCards/>} onEnter={requireAuth}/>
         <Route path="/userlogin" element={<UserLogin />} />
       </Routes>
     </Router>
