c1fcf38844463bd9552d310a632c99168c8476e0
liangxingyan1 <liangxingyan@ufl.edu>
2022-04-08 21:17:35 -0400

    Fixes issues with updating the file

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 //components
 import Header from './components/Header';
 import Home from './components/Home.js';  // Home Page
 import MyClubs from './components/MyClubs.js';
 import MyCards from './components/MyCards.js';
-import UserLogin from './components/UserLogin.js';
 import CreateCard from './components/CreateCard.js';
+import UserLogin from './components/UserLogin.js';
 import About from './components/About.js';
 import ErrorPage from './components/ErrorPage.js';
 //Router
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-
-// function requireAuth(nextState, replace, next) {
-//   if (!authenticated) {
-//     replace({
-//       pathname: "/userlogin",
-//       state: {nextPathname: nextState.location.pathname}
-//     });
-//   }
-//   next();
-// }
+import {userContext} from './Contexts/userContext';
 
 function App() {
   //const [auth] = useState;
+  const [userInfo, setUserInfo] = useState();
+
   return (
-    <Router className="App">
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/myclubs" element={<MyClubs/> }/>
-        <Route path="/mycards" element={<MyCards/>}/>
-        <Route path="/userlogin" element={<UserLogin />} />
-        <Route path="/createcard" element={<CreateCard/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-    </Router>
+    <userContext.Provider value={{userInfo, setUserInfo}}>
+      <Router className="App">
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/myclubs" element={<MyClubs/> }/>
+          <Route path="/mycards" element={<MyCards/>}/>
+          <Route path="/createcard" element={<CreateCard/>} />
+          <Route path="/userlogin" element={<UserLogin />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Router>
+    </userContext.Provider>
   );
 }
 
 export default App;
+
