d3ce1f36663795ca271674c3d43837f988cc0018
tamhuynh3861 <90296519+tamhuynh3861@users.noreply.github.com>
2022-04-17 17:42:31 -0400

    Added live functionality

--- client/src/components/MyClubs.js
+++ client/src/components/MyClubs.js
@@ -9,7 +9,6 @@ import {clubContext} from './../Contexts/clubContext';
 import {Navigate} from 'react-router-dom';
 import Axios from 'axios';
 
-import Card from './Card';
 
 //TO-DO: CHANGE THIS TO USER'S SAVED CLUBS
 const clubs = [
@@ -24,37 +23,42 @@ const MyClubs = () => {
     const {savedClubs} = useContext(clubContext)
     const {setSavedClubs} = useContext(clubContext)
 
-    //if (!userInfo) {
-        //return <Navigate to='./../userlogin'/>
-    //}
+
+    // SECTION TO BE OMITTED FOR LOCAL TESTInG
+    if (!userInfo) {
+        return <Navigate to='./../userlogin'/>
+    }
+    //END OF SECTION
 
     const deleteClub = (id) => {
-        Axios.delete(`http://localhost:3001/delete/${id}`).then(() => {
-          setSavedClubs(savedClubs.filter((val)=> {
-            return val._id != id;
-          }))
-        })
-      }
+        Axios.delete(`http://localhost:3001/delete/${id}`)
 
-    //add different Grid implementation
+        //Axios.delete(`http://localhost:3001/delete/${id}`).then(() => {
+          //setSavedClubs(savedClubs.filter((val)=> {
+            //return val._id != id;
+          //}))
+        //})
+    }
+
+    // Add onClick={deleteClub(club._id)} into code for database functionality
 
     return(
-        <div>
-            <h1 className='title'> My Favorite Club:</h1>
-                <div className='myclub-content'> 
-                    {
-                        savedClubs ? 
-                        savedClubs.map((club) => 
-                            <div classname="club-container">
-                                <div className="club-info">
-                                    <h1> {club.title} </h1>
-                                </div>
-                                <button className="rmv-btn" onClick={deleteClub(club._id)}>remove</button>
-                            </div>
-                        )
-                        : <h1>There are no saved clubs...</h1>
-                    }
-                </div>
+        <div className="SavedContainer">
+            <h3> My Favorite Clubs: </h3>
+            <div className='SavedContent'>
+                {savedClubs.map((club) => 
+                    <div className="rCardContainer">
+                        <div className="rcard-body">
+                            <h4 classname="rTitle">{club.title}</h4>
+                            <p className='rDesc'>{club.desc}</p>
+                        </div>
+                        <div className='btn'>
+                            <button>View More</button>
+                            <button className='rRight-btn'>Remove</button>
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
